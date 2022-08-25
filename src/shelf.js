f15704e18fc818a09864582af630e46e9517e747
Ryan Nagel <RNagel29@gmail.com>
2022-08-25 16:00:43 -0600

    Completed shelf

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,39 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+}
+
+function unshelfBook(book, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (book === shelf[i].title) {
+      shelf.splice(i, 1);
+    }
+  }
+
+}
+
+function listTitles(shelf) {
+  var titleList = [];
+  for (var i = 0; i < shelf.length; i++) {
+    titleList.push(shelf[i].title);
+  }
+  return titleList.join(", ")
+}
+
+function searchShelf(shelf, titleSearched) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (titleSearched === shelf[i].title) {
+      return true;
+    }
+  }
+  return false;
+}
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+shelfBook,
+unshelfBook,
+listTitles,
+searchShelf
 };
